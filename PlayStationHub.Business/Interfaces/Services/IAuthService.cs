1f50edaed2a9d7dcd64d067837b1558c984b427c
FerasBarahmeh <FerasBarahmeh@GLADEATOR>
2024-09-23 20:20:19 +0300

    Create Auth service

---
+++ PlayStationHub.Business/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using PlayStationHub.Business.DataTransferObject.Privileges;
+using PlayStationHub.Business.DataTransferObject.Users;
+
+namespace PlayStationHub.Business.Interfaces.Services;
+
+public interface IAuthService
+{
+    Task<IEnumerable<UserPrivilegeDTO>> Privileges { get; }
+    Task<UserDTO> AuthorizeUser { get; }
+    void Constructing(int id);
+    void Deconstructing();
+}
---
+++ PlayStationHub.Business/Services/AuthService.cs
@@ -0,0 +1,47 @@
+﻿using PlayStationHub.Business.DataTransferObject.Privileges;
+using PlayStationHub.Business.DataTransferObject.Users;
+using PlayStationHub.Business.Interfaces.Services;
+
+namespace PlayStationHub.Business.Services;
+
+public class AuthService : IAuthService
+{
+    public readonly IUserService _userService;
+
+    private IEnumerable<UserPrivilegeDTO> _privileges;
+
+    private int _userID;
+    public AuthService(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public Task<IEnumerable<UserPrivilegeDTO>> Privileges
+    {
+        get
+        {
+            if (_privileges == null && int.IsPositive(_userID))
+                return _userService.GetUserPrivilege(_userID);
+            return null;
+        }
+    }
+
+    public Task<UserDTO> AuthorizeUser
+    {
+        get
+        {
+            if (int.IsNegative(_userID)) return null;
+            return _userService.FindAsync(_userID);
+        }
+    }
+
+
+    public void Constructing(int id)
+    {
+        _userID = id;
+    }
+    public void Deconstructing()
+    {
+        _userID = -1;
+    }
+}
