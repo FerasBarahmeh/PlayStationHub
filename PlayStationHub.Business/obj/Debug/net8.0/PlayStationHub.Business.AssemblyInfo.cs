812ffbacc33bd84716010920238a2aa28bd22dd6
FerasBarahmeh <FerasBarahmeh@GLADEATOR>
2024-09-23 20:19:47 +0300

    creat auth service

--- PlayStationHub.Business/obj/Debug/net8.0/PlayStationHub.Business.AssemblyInfo.cs
+++ PlayStationHub.Business/obj/Debug/net8.0/PlayStationHub.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayStationHub.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e5ef7807c8ce8395cc93e61ed71c945582b0b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayStationHub.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayStationHub.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
